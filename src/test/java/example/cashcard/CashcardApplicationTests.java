3caa917e3f15fca888bcde9399843d898c31f1a8
Kielvien L E S Putra <for2university@gmail.com>
2023-07-27 04:02:05 +0700

    feat: adding rest get by id

--- src/test/java/example/cashcard/CashcardApplicationTests.java
+++ src/test/java/example/cashcard/CashcardApplicationTests.java
@@ -1,11 +1,38 @@
 package example.cashcard;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
-@SpringBootTest
+import com.jayway.jsonpath.DocumentContext;
+import com.jayway.jsonpath.JsonPath;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+//@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class CashcardApplicationTests {
 
+	@Autowired
+    TestRestTemplate restTemplate;
+
+	@Test
+    void shouldReturnACashCardWhenDataIsSaved() {
+        ResponseEntity<String> response = restTemplate.getForEntity("/cashcards/100", String.class);
+
+//        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+//        
+//        DocumentContext documentContext = JsonPath.parse(response.getBody());
+//        Number id = documentContext.read("$.id");
+//        assertThat(id).isNotNull();
+        
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
+  		assertThat(response.getBody()).isBlank();
+    }
+	
 	@Test
 	void contextLoads() {
 	}
