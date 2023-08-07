e05150fb596b27210235ba23d0117ab8f187ed85
Kielvien L E S Putra <for2university@gmail.com>
2023-08-07 16:29:15 +0700

    adding feature get data from h2 database

--- src/main/java/example/cashcard/CashCardController.java
+++ src/main/java/example/cashcard/CashCardController.java
@@ -1,5 +1,7 @@
 package example.cashcard;
 
+import java.util.Optional;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -9,13 +11,26 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/cashcards")
 public class CashCardController {
+
+	private CashCardRepository cashCardRepository;
+
+	public CashCardController(CashCardRepository cashCardRepository) {
+		this.cashCardRepository = cashCardRepository;
+	}
+
 	@GetMapping("/{requestedId}")
-    public ResponseEntity<CashCard> findById(@PathVariable Long requestedId){
-        if (requestedId.equals(99L)) {
-            CashCard cashCard = new CashCard(99L, 123.45);
-            return ResponseEntity.ok(cashCard);
-        } else {
-            return ResponseEntity.notFound().build();
-        }
-    }
+	public ResponseEntity<CashCard> findById(@PathVariable Long requestedId) {
+//		if (requestedId.equals(99L)) {
+//			CashCard cashCard = new CashCard(99L, 123.45);
+//			return ResponseEntity.ok(cashCard);
+//		} else {
+//			return ResponseEntity.notFound().build();
+//		}
+		Optional<CashCard> cashCardOptional = cashCardRepository.findById(requestedId);
+		if (cashCardOptional.isPresent()) {
+			return ResponseEntity.ok(cashCardOptional.get());
+		} else {
+			return ResponseEntity.notFound().build();
+		}
+	}
 }
