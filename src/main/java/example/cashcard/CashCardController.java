0ff387594c6cf2077d04e8070b10381a80f01a70
Kielvien L E S Putra <for2university@gmail.com>
2023-08-08 13:40:16 +0700

    Adding feature find all with sort and pegination

--- src/main/java/example/cashcard/CashCardController.java
+++ src/main/java/example/cashcard/CashCardController.java
@@ -3,6 +3,10 @@ package example.cashcard;
 import java.net.URI;
 import java.util.Optional;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -45,5 +49,20 @@ public class CashCardController {
 
 //		return ResponseEntity.created(URI.create("/what/should/go/here?")).build();
 		return ResponseEntity.created(locationOfNewCashCard).build();
+	}
+
+//	@GetMapping
+//	public ResponseEntity<Iterable<CashCard>> findAll() {
+//		return ResponseEntity.ok(cashCardRepository.findAll());
+//	}
+
+	@GetMapping
+	public ResponseEntity<Iterable<CashCard>> findAll(Pageable pageable) {// return
+																			// ResponseEntity.ok(cashCardRepository.findAll());
+		Page<CashCard> page = cashCardRepository.findAll(PageRequest.of(pageable.getPageNumber(),
+				pageable.getPageSize(), pageable.getSortOr(Sort.by(Sort.Direction.ASC, "amount"))));
+
+		return ResponseEntity.ok(page.getContent());
+
 	}
 }
